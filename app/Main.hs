c6341b90af10c0439077917fac5d7976acd1b2b6
Nadya Hoesin <nadya.hoesin@ui.ac.id>
2024-11-03 16:20:44 +0700

    backend with json api

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import SentimentAnalyzer (main)
+import Web.Scotty
+import SentimentAnalyzer (sentimentAnalyzer, naiveBayesClassifier, preprocessTrainingData, evaluateAccuracy)
+import qualified Data.Text.Lazy as TL
+import Data.Aeson (object, (.=))
+import Data.Maybe (fromMaybe)
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+import Data.Csv (decode, HasHeader(NoHeader))
+
+type TextSentiment = (Int, String)
 
 main :: IO ()
-main = SentimentAnalyzer.main
+main = do
+    trainVector <- loadTrainingData "updated_training.csv"
+    let classFreqs = preprocessTrainingData trainVector
+
+    scotty 8080 $ do
+        get "/" $ do
+            text <- param "text"
+            let sentiment = if naiveBayesClassifier (TL.unpack text) classFreqs == 4 then TL.pack "positive" else TL.pack "negative"
+            json $ object ["text" .= text, "sentiment" .= sentiment]
+
+loadTrainingData :: FilePath -> IO (V.Vector TextSentiment)
+loadTrainingData filePath = do
+    csvData <- BL.readFile filePath
+    case decode NoHeader csvData of
+        Left err -> error err
+        Right v -> return v
+
--- src/SentimentAnalyzer.hs
+++ src/SentimentAnalyzer.hs
@@ -1,4 +1,4 @@
-module SentimentAnalyzer (main) where
+module SentimentAnalyzer (sentimentAnalyzer, naiveBayesClassifier, preprocessTrainingData, evaluateAccuracy) where
 
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv ( decode, HasHeader(NoHeader) )
@@ -11,8 +11,8 @@ import Data.List (intersect)
 type TextSentiment = (Int, String)
 type WordFreqsByClass = (Map.Map String Int, Map.Map String Int)
 
-main :: IO ()
-main = do
+sentimentAnalyzer :: IO ()
+sentimentAnalyzer = do
     -- Membaca dataset
     csvData <- BL.readFile "updated_training.csv"
     case decode NoHeader csvData of
@@ -22,15 +22,15 @@ main = do
             -- print $ length $ fst $ preprocessTrainingData trainVector
             -- print $ length $ fst $ removeUninformativeTokens $ preprocessTrainingData trainVector
 
-            let classFreqs@(posFreq, negFreq) = removeUninformativeTokens $ preprocessTrainingData trainVector
+            let classFreqs = preprocessTrainingData trainVector
 
             -- putStrLn "Positive Token Frequencies:"
             -- print posFreq
             -- putStrLn "Negative Token Frequencies:"
             -- print negFreq
 
-            putStr "Sentiment of \"Really happy happy but sad\": "
-            putStrLn $ if naiveBayesClassifier "Really happy happy but sad" classFreqs == 4 then "positive" else "negative"
+            putStr "Sentiment of \"Going on holiday this weekend\": "
+            putStrLn $ if naiveBayesClassifier "Going on holiday this weekend" classFreqs == 4 then "positive" else "negative"
 
             putStr "Sentiment of \"Why is my code not working\": "
             putStrLn $ if naiveBayesClassifier "Why is my code not working" classFreqs == 4 then "positive" else "negative"
@@ -39,11 +39,15 @@ main = do
             print $ evaluateAccuracy testVector classFreqs
 
 
+{-----------------------------------------------------------------------------------------------------------------------------------------------------                               
+                                                    PREPROCESSING FUNCTIONS
+------------------------------------------------------------------------------------------------------------------------------------------------------}
+
 -- Mengubah 1 vektor berisi semua data menjadi tuple berisi map token ke jumlah kemunculan token untuk tiap kelas
 preprocessTrainingData :: V.Vector TextSentiment -> WordFreqsByClass
-preprocessTrainingData = tMap (countTokensFreq . concatMap tokenize) . separateByClass
+preprocessTrainingData = removeUninformativeTokens . tMap (countTokensFreq . concatMap tokenize) . separateByClass
 
--- Mengubah 1 vektor berisi semua data menjadi tuple berisi 3 lists
+-- Mengubah 1 vektor berisi semua data menjadi tuple berisi 2 lists
 -- list 1: semua teks dengan sentimen positif
 -- list 2: semua teks dengan sentimen negatif
 separateByClass :: V.Vector TextSentiment -> ([String], [String])
@@ -57,19 +61,32 @@ tokenize = words . map toLower . filter (\c -> isAlpha c || c == ' ')
 countTokensFreq :: [String] -> Map.Map String Int
 countTokensFreq = foldr (\token -> Map.insertWith (+) token 1) Map.empty
 
--- Menghapus token yang sering tetapi frequensi rata di tiap kelas
+-- Menghapus token yang sangat sering tetapi frequensi rata di tiap kelas, atau token yang sangat jarang
 removeUninformativeTokens :: WordFreqsByClass -> WordFreqsByClass
 removeUninformativeTokens freqs@(pos, neg) = tMap (Map.filter (> 5))  
                                              (foldr Map.delete pos uninformativeTokens, foldr Map.delete neg uninformativeTokens)
+    where uninformativeTokens = uncurry intersect $ tMap (Map.keys . Map.filter (> 450)) freqs
+
+{-----------------------------------------------------------------------------------------------------------------------------------------------------                               
+                                                    NAIVE BAYES FUNCTIONS
+------------------------------------------------------------------------------------------------------------------------------------------------------}
+
+-- Naive Bayes classifier, memprediksi sentimen teks ketika diberi teks dan frekuensi kata tiap kelas
+naiveBayesClassifier :: String -> WordFreqsByClass -> Int
+naiveBayesClassifier text freqs
+    | probPos >= probNeg = 4
+    | otherwise = 0
     where
-        uninformativeTokens = uncurry intersect $ tMap (Map.keys . Map.filter (> 500)) freqs
+        (probPos, probNeg) = foldl1 (tZipWith (+)) [tZipWith (*) (computeTokenGivenClassProbs token freqs) classProbs | token <- tokenize text]
+        classProbs = computeClassProbs freqs
+        tZipWith f (x1, y1) (x2, y2) = (f x1 x2, f y1 y2)
 
 -- Mendapatkan probabilitas tiap kelas
 computeClassProbs :: WordFreqsByClass -> (Float, Float)
 computeClassProbs (pos, neg) =  (getNumOfToken pos / numOfAllTokens, getNumOfToken neg / numOfAllTokens)
     where numOfAllTokens = getNumOfToken pos + getNumOfToken neg
 
--- Mendapatkan probabilitas untuk token given kelas untuk tiap kelas
+-- Mendapatkan probabilitas untuk token given kelas untuk tiap kelas (dengan laplacian smoothing)
 computeTokenGivenClassProbs :: String -> WordFreqsByClass -> (Float, Float)
 computeTokenGivenClassProbs token (pos, neg) = ((countToken pos + 0.5) / (getNumOfToken pos + 1), (countToken neg + 0.5) / (getNumOfToken neg + 1))
     where countToken = fromIntegral . fromMaybe 0 . Map.lookup token
@@ -78,21 +95,12 @@ computeTokenGivenClassProbs token (pos, neg) = ((countToken pos + 0.5) / (getNum
 getNumOfToken :: Map.Map k Int -> Float
 getNumOfToken = fromIntegral . Map.foldr (+) 0
 
--- Naive Bayes classifier, diberi text dan memprediksi sentimen text tersebut
-naiveBayesClassifier :: String -> WordFreqsByClass -> Int
-naiveBayesClassifier text freqs
-    | probPos >= probNeg = 4
-    | otherwise = 0
-    where
-        (probPos, probNeg) = foldl1 (tZipWith (+))
-                             [tZipWith (*) (computeTokenGivenClassProbs token freqs) classProbs | token <- tokenize text]
-        classProbs = computeClassProbs freqs
-        tZipWith f (x1, y1) (x2, y2) = (f x1 x2, f y1 y2)
-
 -- Mengevaluasi akurasi Naive Bayes classifier terhadap testing data
 evaluateAccuracy :: V.Vector TextSentiment -> WordFreqsByClass -> Float
 evaluateAccuracy textSentiments freqs = numOfAccurate / fromIntegral (length textSentiments)
     where numOfAccurate = sum [1 | (sentiment, text) <- V.toList textSentiments, naiveBayesClassifier text freqs == sentiment]
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------                               
 
 -- Map function over tuple with 2 elements
 tMap :: (a -> b) -> (a, a) -> (b, b)
